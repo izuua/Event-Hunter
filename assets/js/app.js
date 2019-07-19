691b051138b54ed3c039a8a2e5da12ae95625496
Caleb Myers <cmyers5108@gmail.com>
2019-07-19 15:09:13 -0400

    integrate search fn onto index.html, including some exception handling

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,37 +1,56 @@
 // Starts a query when the "Search" button is clicked
-$("#js-btn-search").on("click", function () {
+$("#js-btn-search").on("click", function (event) {
+
+    //prevent default
+    //call checkInput
+    //if .val() === ""
+
+    event.preventDefault()
     var keyword = $("#js-input-search").val().trim()
-    $("#js-input-search").val("")
+    // Clears old search results
+    $("#js-results").empty()
 
-    var queryURL = "https://app.ticketmaster.com/discovery/v2/events?apikey=1CDZF2AkHAO8FPwY0r3kQm6bmxI7Vuk5&keyword=" + keyword + "&locale=*&includeSpellcheck=yes"
+    if (keyword === "") {
+        // $("#js-input-search").val("")
+        $("#js-form-search")[0].reset();
+        $("#js-display-row").hide()
+        console.log("works", keyword)
+        console.log("didnt search")
+    } else {
+        console.log("did search")
+        $("#js-display-input").text(keyword)
+        $("#js-display-row").show()
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (res) {
-        var searchData = res._embedded
-        console.log(res)
-        console.log(searchData)
+        var queryURL = "https://app.ticketmaster.com/discovery/v2/events?apikey=1CDZF2AkHAO8FPwY0r3kQm6bmxI7Vuk5&keyword=" + keyword + "&locale=*&includeSpellcheck=yes"
 
-        // Clears old search results
-        $("#js-results").empty()
 
-        // Creates cards for each matching result
-        for (let i = 0; i < searchData.events.length; i++) {
-            var newCard = $("<div class='col-12 col-md-6 col-lg-3'>").append(
-                $("<a href='#' class='card-link' data-event-id='" + searchData.events[i].id + "'>").append(
-                    $("<div class='card'>").append(
-                        $("<img src='" + searchData.events[i].images[0].url + "' alt='" + searchData.events[i].name + "' class='card-img-top'>"),
-                        $("<div class='card-body'>").append(
-                            $("<h5 class='card-title'>").text(searchData.events[i].name),
-                            $("<p class='card-text'>").text(searchData.events[i].dates.start.localDate),
-                            $("<p class='card-text'>").text(searchData.events[i]._embedded.venues[0].name)
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (res) {
+            var searchData = res._embedded
+            console.log(res)
+            console.log(searchData)
+
+            // add exception handling for no results
+
+            // Creates cards for each matching result
+            for (let i = 0; i < searchData.events.length; i++) {
+                var newCard = $("<div class='col-12 col-md-6 col-lg-3'>").append(
+                    $("<a href='#' class='card-link' data-event-id='" + searchData.events[i].id + "'>").append(
+                        $("<div class='card'>").append(
+                            $("<img src='" + searchData.events[i].images[0].url + "' alt='" + searchData.events[i].name + "' class='card-img-top'>"),
+                            $("<div class='card-body'>").append(
+                                $("<h5 class='card-title'>").text(searchData.events[i].name),
+                                $("<p class='card-text'>").text(searchData.events[i].dates.start.localDate),
+                                $("<p class='card-text'>").text(searchData.events[i]._embedded.venues[0].name)
+                            )
                         )
                     )
                 )
-            )
 
-            $("#js-results").append(newCard)
-        }
-    })
+                $("#js-results").append(newCard)
+            }
+        })
+    }
 })
