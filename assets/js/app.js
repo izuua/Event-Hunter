4eec3f0c3672e1ae170f2331ed7a470801c02b31
Cassidy G <cassidygroenendaal@gmail.com>
2019-07-21 15:10:39 -0400

    Allow users to search from details.html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,121 +1,197 @@
+//==================================================
+// VARIABLES
+//--------------------------------------------------
+
 // Starts a query when the "Search" button is clicked
+var searchData;
 
-var searchData
+var siteURL = 'http://127.0.0.1:5501/Project-1/';
 
-$("#js-btn-search").on("click", function (event) {
-    event.preventDefault()
+//==================================================
+// FUNCTIONS
+//--------------------------------------------------
 
-    var keyword = $("#js-input-search").val().trim()
+function initMap() {
+	// The location of the venue
+	var location = {
+		lat : parseFloat(localStorage.getItem('lat')),
+		lng : parseFloat(localStorage.getItem('lng'))
+	};
+	// The map, centered at the venue
+	var map = new google.maps.Map(document.getElementById('js-map'), {
+		zoom   : 15,
+		center : location
+	});
+	// The marker, positioned at the venue
+	var marker = new google.maps.Marker({
+		position : location,
+		map      : map
+	});
+}
 
-    // Clears old search results
-    $("#js-results").empty()
-    $("#js-form-search")[0].reset();
+function searchDetails() {
+	var searchKeyword = $('#js-input-details-search').val().trim();
+	localStorage.setItem('searchKeyword', searchKeyword);
+	location.href = siteURL + 'index.html';
+}
 
-    // Checks if user made an input
-    if (keyword === "") {
-        console.log("didnt search")
-        $("#js-display-row").hide()
-    } else {
-        console.log("did search")
-        $("#js-display-input").text(keyword)
-        $("#js-display-row").show()
+function mainSearch() {
+	var keyword = $('#js-input-search').val().trim();
 
-        var queryURL = "https://app.ticketmaster.com/discovery/v2/events?apikey=1CDZF2AkHAO8FPwY0r3kQm6bmxI7Vuk5&keyword=" + keyword + "&locale=*&includeSpellcheck=yes"
+	// Clears old search results
+	$('#js-results').empty();
+	$('#js-form-search')[0].reset();
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (res) {
-            searchData = res._embedded
-            console.log(res)
-            console.log(searchData)
+	// Checks if user made an input
+	if (keyword === '') {
+		console.log('didnt search');
+		$('#js-display-row').hide();
+	} else {
+		console.log('did search');
+		$('#js-display-input').text(keyword);
+		$('#js-display-row').show();
 
-            // add exception handling for no results
+		var queryURL =
+			'https://app.ticketmaster.com/discovery/v2/events?apikey=1CDZF2AkHAO8FPwY0r3kQm6bmxI7Vuk5&keyword=' +
+			keyword +
+			'&locale=*&includeSpellcheck=yes';
 
-            //if (searchData === undefined || res._embedded === null) {
-            //shows div saying "No results found"
-            //return 0;
-            // }
+		$.ajax({
+			url    : queryURL,
+			method : 'GET'
+		}).then(function(res) {
+			searchData = res._embedded;
+			console.log(res);
+			console.log(searchData);
 
-            
-            // Creates cards for each matching result
-            for (let i = 0; i < searchData.events.length; i++) {
-                console.log("card making running");
-                var newCard = $("<div class='col-12 col-md-6 col-lg-3'>").append(
-                    $("<a href='./details.html' target=_blank class='card-link' data-event-id='" + searchData.events[i].id + "' data-event-lat='"+ searchData.events[i]._embedded.venues[0].location.latitude + "' data-event-lng='" + searchData.events[i]._embedded.venues[0].location.longitude + "'>").append(
-                        $("<div class='card'>").append(
-                            $("<img src='" + searchData.events[i].images[0].url + "' alt='" + searchData.events[i].name + "' class='card-img-top'>"),
-                            $("<div class='card-body'>").append(
-                                $("<h5 class='card-title'>").text(searchData.events[i].name),
-                                $("<p class='card-text'>").text(searchData.events[i].dates.start.localDate),
-                                $("<p class='card-text'>").text(searchData.events[i]._embedded.venues[0].name)
-                            )
-                        )
-                    )
-                )
-                $("#js-results").append(newCard)
-            }
-        })
-    }
-})
+			// add exception handling for no results
+
+			//if (searchData === undefined || res._embedded === null) {
+			//shows div saying "No results found"
+			//return 0;
+			// }
+
+			// Creates cards for each matching result
+			for (let i = 0; i < searchData.events.length; i++) {
+				console.log('card making running');
+				var newCard = $(
+					"<div class='col-12 col-md-6 col-lg-3'>"
+				).append(
+					$(
+						"<a href='./details.html' target=_blank class='card-link' data-event-id='" +
+							searchData.events[i].id +
+							"' data-event-lat='" +
+							searchData.events[i]._embedded.venues[0].location
+								.latitude +
+							"' data-event-lng='" +
+							searchData.events[i]._embedded.venues[0].location
+								.longitude +
+							"'>"
+					).append(
+						$("<div class='card'>").append(
+							$(
+								"<img src='" +
+									searchData.events[i].images[0].url +
+									"' alt='" +
+									searchData.events[i].name +
+									"' class='card-img-top'>"
+							),
+							$("<div class='card-body'>").append(
+								$("<h5 class='card-title'>").text(
+									searchData.events[i].name
+								),
+								$("<p class='card-text'>").text(
+									searchData.events[i].dates.start.localDate
+								),
+								$("<p class='card-text'>").text(
+									searchData.events[i]._embedded.venues[0].name
+								)
+							)
+						)
+					)
+				);
+				$('#js-results').append(newCard);
+			}
+		});
+	}
+}
+
+//==================================================
+// APP INIT
+//--------------------------------------------------
+
+$('#js-btn-search').on('click', function(e) {
+	e.preventDefault();
+	mainSearch();
+});
 
 // Stores data needed for details.html
-$(document).on("click", ".card-link", function () {
-    // Store the event id, lat and lng
-    var id = $(this).attr("data-event-id")
-    var lat = $(this).attr("data-event-lat")
-    var lng = $(this).attr("data-event-lng")
-    localStorage.clear()
-    localStorage.setItem("id", id)
-    localStorage.setItem("lat", lat)
-    localStorage.setItem("lng", lng)
-})
+$('#js-results').on('click', '.card-link', function() {
+	// Store the event id, lat and lng
+	var id = $(this).attr('data-event-id');
+	var lat = $(this).attr('data-event-lat');
+	var lng = $(this).attr('data-event-lng');
+	localStorage.clear();
+	localStorage.setItem('id', id);
+	localStorage.setItem('lat', lat);
+	localStorage.setItem('lng', lng);
+});
 
 // Gets event details after the user selects an event and moves to details.html
-$(document).ready(function () {
-    // Current brower href
-    var pageRef = window.location.href
+$(document).ready(function() {
+	// Current brower href
+	var pageRef = window.location.href;
 
-    // Checks if details.html or index.html is active
-    if (pageRef.search("details") === -1) {
-        console.log("You are on the index page")
-    } else {
-        console.log("Details page!!!")
-        var detailsID = localStorage.getItem("id")
-        var queryURL = "https://app.ticketmaster.com/discovery/v2/events/" + detailsID + "?apikey=1CDZF2AkHAO8FPwY0r3kQm6bmxI7Vuk5&locale=*&includeSpellcheck=yes"
+	// Checks if details.html or index.html is active
+	if (pageRef.search('details') === -1) {
+		console.log('You are on the index page');
+		if (localStorage.getItem('searchKeyword')) {
+			$('#js-input-search').val(
+				localStorage.getItem('searchKeyword')
+			);
+			mainSearch();
+			localStorage.removeItem('searchKeyword');
+		}
+	} else {
+		console.log('Details page!!!');
+		$('#js-btn-details-search').on('click', function(e) {
+			e.preventDefault();
+			searchDetails();
+		});
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (res) {
-            searchData = res._embedded
-            console.log(res)
-            console.log(searchData)
+		var detailsID = localStorage.getItem('id');
+		var queryURL =
+			'https://app.ticketmaster.com/discovery/v2/events/' +
+			detailsID +
+			'?apikey=1CDZF2AkHAO8FPwY0r3kQm6bmxI7Vuk5&locale=*&includeSpellcheck=yes';
 
-            // Adds info to the brief section
-            $("#js-brief-event").text(res.name)
-            $("#js-brief-date").text(res.dates.start.localDate)
-            $("#js-brief-time").text(moment(res.dates.start.localTime, "HH:mm:SS").format("hh:mm A"))
-            $("#js-brief-location").text(res._embedded.venues[0].name)
-            // Adds info to the details section
-            $("#js-details-event").text(res.name)
-            $("#js-details-date").text(res.dates.start.localDate)
-            $("#js-details-location").text(res._embedded.venues[0].name)
-            $("#js-details-tickets").html("<a href=" + res.url + " target='_blank'>Click Here")
-            $("#js-details-genre").text(res.classifications[0].genre.name)
+		$.ajax({
+			url    : queryURL,
+			method : 'GET'
+		}).then(function(res) {
+			searchData = res._embedded;
+			console.log(res);
+			console.log(searchData);
 
-        })
-    }
-})
-
+			// Adds info to the brief section
+			$('#js-brief-event').text(res.name);
+			$('#js-brief-date').text(res.dates.start.localDate);
+			$('#js-brief-time').text(
+				moment(res.dates.start.localTime, 'HH:mm:SS').format(
+					'hh:mm A'
+				)
+			);
+			$('#js-brief-location').text(res._embedded.venues[0].name);
+			// Adds info to the details section
+			$('#js-details-event').text(res.name);
+			$('#js-details-date').text(res.dates.start.localDate);
+			$('#js-details-location').text(res._embedded.venues[0].name);
+			$('#js-details-tickets').html(
+				'<a href=' + res.url + " target='_blank'>Click Here"
+			);
+			$('#js-details-genre').text(res.classifications[0].genre.name);
+		});
+	}
+});
 
 // // Initialize and add the map
-function initMap() {
-    // The location of the venue
-    var location = { lat: parseFloat(localStorage.getItem("lat")), lng: parseFloat(localStorage.getItem("lng")) };
-    // The map, centered at the venue
-    var map = new google.maps.Map(
-        document.getElementById('js-map'), { zoom: 15, center: location });
-    // The marker, positioned at the venue
-    var marker = new google.maps.Marker({ position: location, map: map });
-}
